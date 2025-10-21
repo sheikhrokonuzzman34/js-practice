604bac6c167881bf9a7ae05e55f2441818c7383d
sheikhrokonuzzman34 <mdrokon.u545@gmail.com>
2025-10-21 12:38:06 +0600

    object and method , data type object

--- js/style.js
+++ js/style.js
@@ -1,11 +1,76 @@
 
-function sleep(){
-    console.log("My name is zubayer " )
-}
+// function sleep(){
+//     console.log("My name is zubayer " )
+// }
 
-sleep('Rokon')
-function sleep(name){
-    console.log("My name is " + name )
-}
+// sleep('Rokon')
+// function sleep(name){
+//     console.log("My name is " + name )
+// }
 
-sleep('Rokon')
+// sleep('Rokon')
+
+// Convert Fahrenheit to Celsius
+// function toCelsius(fahrenheit) {
+//     return (fahrenheit - 32) * 5 / 9;
+// }
+
+// let x = toCelsius(97)
+// console.log(x)
+
+// // Optional: Convert Celsius to Fahrenheit
+// function toFahrenheit(celsius) {
+//     return (celsius * 9 / 5) + 32;
+// }
+
+// let y = toFahrenheit(36.111111111111114)
+// console.log(y)
+
+// var carName = "BMW"
+
+// function myFunction(){
+//     let carName = "volvo";
+//     console.log(carName)
+// }
+
+// myFunction()
+
+// confirm.log(carName)
+
+// object
+// const car = {
+//     model:"BMWX",
+//     color:"read",
+//     stard : function() {
+//         console.log("car has stared")
+//     },
+//     drive : function() {
+//         console.log("car is driving")
+//     }
+// }
+
+// console.log(car.model)
+// console.log(car['color'])
+// car.stard()
+
+
+// const person = {
+//     fName:"Sheikh",
+//     lName:"Rokon",
+//     id:123456,
+//     fullName: function(){
+//         return this.fName + " " + this.lName;
+//     }
+// }
+
+// console.log(person.fullName())
+
+let x = 5
+
+let  y= new String('hello')
+
+console.log(typeof x)
+console.log(typeof y)
+
+
+
